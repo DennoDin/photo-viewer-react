d456a90158ec8f367617519322de77fdcd380309
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-08-01 16:02:38 +0900

    Configured state, componentDidMount, onClick, and render methods
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Tsuyoshi Akiyama <ka090824@gmail.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import Navbar from "./Navbar";
+import AllPhotos from "./AllPhotos";
+import SinglePhoto from "./SinglePhoto";
+import { listObjects, getSingleObject, saveObject } from "../utils/index";
 
 import "../styles/styles.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentView: "AllPhotos",
+      photos: [],
+      selectedPhoto: "",
+    };
   }
 
+  componentDidMount() {
+    const photos = listObjects();
+    photos.then((pics) => {
+      this.setState({ photos: [...pics] });
+    });
+  }
+
+  onClick = () => {
+    this.setState({ currentView: "AllPhotos" });
+    console.log("clicked here");
+  };
+
   render() {
-    return <div className="app" />;
+    //TODO set conditions later
+    if (this.state.currentView === "AllPhotos") {
+      return (
+        <div className="app">
+          <Navbar onClick={this.onClick} />put all photos here
+        </div>
+      );
+    }
+    return (
+      <div className="app">
+        <Navbar />put single photos here
+      </div>
+    );
   }
 }
